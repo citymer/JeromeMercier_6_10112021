86983cc911acddf8537be57c79cc61085a3ed8c1
citymer <cityhunter8484@gmail.com>
2021-11-18 10:24:39 +0100

    ajout img des photographes

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -8,17 +8,59 @@
                 "country": "UK",
                 "tagline": "Voir le beau dans le quotidien",
                 "price": 400,
-                "portrait": "MimiKeel.jpg"
+                "portrait": "MimiKeel.jpg",
+                "alt": "portrait de Mimi Keel"
             },
             {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
+                "name": "Ellie-Rose Wilkens",
+                "id": 930,
+                "city": "Paris",
+                "country": "France",
+                "tagline": "Capturer des compositions complexes",
+                "price": 250,
+                "portrait": "EllieRoseWilkens.jpg",
+                "alt": "portrait de Ellie-Rose Wilkens"
             },
+            {
+                "name": "Tracy Galindo",
+                "id": 82,
+                "city": "Montreal",
+                "country": "Canada",
+                "tagline": "Photographe freelance",
+                "price": 500,
+                "portrait": "TracyGalindo.jpg",
+                "alt":"portrait de Tracy Galindo"
+            },
+            {
+                "name": "Nabeel Bradford",
+                "id": 527,
+                "city": "Mexico City",
+                "country": "Mexico",
+                "tagline": "Toujours aller de l'avant",
+                "price": 350,
+                "portrait": "NabeelBradford.jpg",
+                "alt": "portrait de Nabeel Bradford"
+            },
+            {
+                "name": "Rhode Dubois",
+                "id": 925,
+                "city": "Barcelona",
+                "country": "Spain",
+                "tagline": "Je crée des souvenirs",
+                "price": 275,
+                "portrait": "RhodeDubois.jpg",
+                "alt": "portrait de Rhode Dubois"
+            },
+            {
+                "name": "Marcel Nikolic",
+                "id": 195,
+                "city": "Berlin",
+                "country": "Germany",
+                "tagline": "Toujours à la recherche de LA photo",
+                "price": 300,
+                "portrait": "MarcelNikolic.jpg",
+                "alt": "portrait de Marcel Nikolic"
+            }
         ]
         // et bien retourner le tableau photographers seulement une fois
         return ({
@@ -42,4 +84,4 @@
     };
     
     init();
-    
+   
