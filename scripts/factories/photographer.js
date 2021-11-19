28eb23b9f7dbb8e5f9585c622fda60d47dc2712a
citymer <cityhunter8484@gmail.com>
2021-11-19 15:06:48 +0100

    rajout balise <a> et espace entre city/country

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,22 +1,25 @@
 function photographerFactory(data) {
-    const { name, portrait, city, country, tagline, price} = data;
+    const { name, portrait, city, country, tagline, price,alt} = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const a = document.createElement('a');
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt",alt)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         const h3 = document.createElement('h3');
-        h3.textContent = [city,country];
+        h3.textContent = `${city}, ${country}`;
         const h4 = document.createElement('h4');
         h4.textContent = tagline;
         const p = document.createElement('p');
         p.textContent = price + "€/jour";
-        article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(a);
+        a.appendChild(img);
+        a.appendChild(h2);
         article.appendChild(h3);
         article.appendChild(h4);
         article.appendChild(p);
