52564f2de2ffb847508f4ea6f29fbaf54bbd2220
citymer <cityhunter8484@gmail.com>
2021-12-06 15:39:44 +0100

    modification du DOM photographers

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,55 +1,46 @@
 //Mettre le code JavaScript lié à la page photographer.html
-async function getPhotographers() {  
-    
-    const photographers = [];
-    const media = [];
+const article = document.querySelector('article');
+const h2 = document.createElement('h2');
+h2.textContent = 'Mimi Keel';
+const h3 = document.createElement('h3');
+h3.textContent = 'London, UK';
+const h4 = document.createElement('h4');
+h4.textContent = 'Voir le beau dans le quotidien';
+article.appendChild(h2);
+article.appendChild(h3);
+article.appendChild(h4);
+console.log(h2);
 
-    fetch('data/photographers.json')
 
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("HTTP error " + response.status);
-        }
-        return response.json();  
-        console.log(json)   
-    })
 
-    .then(json => {
-        this.photographers = json.photographers;
-        this.media = json.media;
+/*  const photographers = [];
+const media = [];*/
+
+
+fetch('data/photographers.json')
+
+.then(response => {
+    if (!response.ok) {
+        throw new Error("HTTP error " + response.status);
+    }
+    return response.json();  
+})
+
+.then(json => {
+    this.photographers = json.photographers[0].name;
+    this.media = json.media;
+        console.log(this.photographers)   
     })
 
     .catch(function () {
         this.dataError = true;
     })  
+
+ 
+
     
-    return ({
-        photographers : [this.photographers],
-        media : [this.media]
-    })
-}
-
-async function init() {
-
-    // Récupère les datas des photographes
-    const { photographers } = await getPhotographers();
-    displayData(photographers);
-};
-
-init();
-
-function photographerFactory(data) {
-
-    const { name, portrait, city, country, tagline, price,alt,id,photographerId,title,image,likes,date} = data;
-
-    const picture = `assets/photographers/${portrait}`;
-    const image = ~assets/images/~
-
-    function getUserCardDOM() {
         
-     
 
-        return (article);
-    }
-    return { getUserCardDOM }
-}
+     
+    
+    
