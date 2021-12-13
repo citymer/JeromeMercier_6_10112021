c63aa0efd4ee61bfc91a544ae623672c77471e7f
citymer <cityhunter8484@gmail.com>
2021-12-13 16:20:57 +0100

    rangements du DOM

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -28,12 +28,15 @@ const sectionPhotographHeader = document.getElementById('photograph-header');
 
 // SECTION sectionphotographHeader
 const article = document.createElement('article');
-const h2 = document.createElement('h2');
-h2.textContent = 'Mimi Keel';
-const h3 = document.createElement('h3');
-h3.textContent = 'London, UK';
-const h4 = document.createElement('h4');
-h4.textContent = 'Voir le beau dans le quotidien';
+
+   const h2 = document.createElement('h2');
+   h2.textContent = 'Mimi Keel';
+
+   const h3 = document.createElement('h3');
+   h3.textContent = 'London, UK';
+
+   const h4 = document.createElement('h4');
+   h4.textContent = 'Voir le beau dans le quotidien';
 
 const button = document.querySelector('.contact_button');
 
@@ -57,37 +60,38 @@ const main = document.getElementById('main');
 const sectionTrier = document.createElement('section');
 sectionTrier.id = 'filter';
 main.appendChild(sectionTrier);
-const label = document.createElement('label');
-label.textContent = 'Trier par';
-label.setAttribute("for","trier");
-sectionTrier.appendChild(label);
 
-const div = document.createElement('div');
-div.setAttribute("class","navBar");
-sectionTrier.appendChild(div);
+   const label = document.createElement('label');
+   label.textContent = 'Trier par';
+   label.setAttribute("for","trier");
+   sectionTrier.appendChild(label);
 
-const ulMenu = document.createElement('ul');
-ulMenu.id = 'trier';
-div.appendChild(ulMenu);
+   const div = document.createElement('div');
+   div.setAttribute("class","navBar");
+   sectionTrier.appendChild(div);
 
-const liPopularite = document.createElement('li');
-liPopularite.setAttribute("class","popularite");
-ulMenu.appendChild(liPopularite);
-liPopularite.textContent = 'Popularité';
+      const ulMenu = document.createElement('ul');
+      ulMenu.id = 'trier';
+      div.appendChild(ulMenu);
 
-const ul = document.createElement('ul');
-ul.setAttribute("class","sousmenu");
-liPopularite.appendChild(ul);
+        const liPopularite = document.createElement('li');
+        liPopularite.setAttribute("class","popularite");
+        ulMenu.appendChild(liPopularite);
+        liPopularite.textContent = 'Popularité';
 
-const liDate = document.createElement('li');
-liDate.setAttribute("class","date");
-ul.appendChild(liDate);
-liDate.textContent = 'Date';
+           const ul = document.createElement('ul');
+           ul.setAttribute("class","sousmenu");
+           liPopularite.appendChild(ul);
+
+               const liDate = document.createElement('li');
+               liDate.setAttribute("class","date");
+               ul.appendChild(liDate);
+               liDate.textContent = 'Date';
     
-const liTitre = document.createElement('li');
-liTitre.setAttribute("class","titre");
-ul.appendChild(liTitre);
-liTitre.textContent = 'Titre';
+               const liTitre = document.createElement('li');
+               liTitre.setAttribute("class","titre");
+               ul.appendChild(liTitre);
+               liTitre.textContent = 'Titre';
         
 // DIV contenant les photos et la video des photographes
 
