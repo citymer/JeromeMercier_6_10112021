ff31e3a5f419f9394d7803322828c5530c284435
citymer <cityhunter8484@gmail.com>
2021-12-23 15:50:22 +0100

    creation fonction attachinformationmedia

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -8,6 +8,7 @@
     const urlParams = new URLSearchParams (getId);
     const idPhotograph = urlParams.get ('id');
     const filterPhotographer = [];
+    const filterMedia = [];
      
     
     // rapatrie les données de photographers.json
@@ -28,10 +29,10 @@
        this.filterPhotographer = photographers.filter((photographer) => photographer.id == idPhotograph);
    
        // filtre media avec photographerId
-       const filterMedia = media.filter((media) => media.photographerId == idPhotograph);
+       this.filterMedia = media.filter((media) => media.photographerId == idPhotograph);
        
        //  retourne le tableau photographers seulement une fois
-           return filterPhotographer;
+           return filterPhotographer,filterMedia;
 
        
 
@@ -45,7 +46,7 @@
 
 
 
-function attachInformation(photographer) {
+function attachInformationPhotograph(photographer) {
     console.log(photographer);
 
    const { name, portrait, city, country, tagline,alt,id} = this.filterPhotographer[0];
@@ -56,7 +57,7 @@ function attachInformation(photographer) {
           const header = document.querySelector('header');
               const aLienAcceuil = document.createElement('a');
               aLienAcceuil.setAttribute("href","./index.html");
-              
+
               const imgLogo = document.querySelector('.logo');
 
               header.appendChild(aLienAcceuil);
@@ -142,48 +143,50 @@ main.appendChild(sectionTrier);
               ul.appendChild(liTitre);
               liTitre.textContent = 'Titre';
        
-   // DIV contenant les photos et la video des photographes
+              
+ }  
+            
+function attachInformationMedia(media) {
 
-   const {title,likes,image} = filterMedia[0];
-
-   const images = `assets/images/${image}`;
-
-   const divPhoto = document.createElement('div');
-   divPhoto.setAttribute("class","divphoto");
-   main.appendChild(divPhoto);
-   
-   const articlePhoto = document.createElement('article');
-   divPhoto.appendChild(articlePhoto);
-   
-   
-         const imgArticle = document.createElement('img');
-         articlePhoto.appendChild(imgArticle);
-         imgArticle.setAttribute("class","imgarticle");
-         imgArticle.setAttribute("src","assets/images/Rhode/Sport_2000_with_8.jpg");
-         imgArticle.setAttribute("alt",alt);
-   
+            const {title,likes,image} = this.filterMedia[0];
          
-         const divquantityheart = document.createElement('div');
-         divquantityheart.setAttribute("class","quantityHeart");
-         articlePhoto.appendChild(divquantityheart);
+            const images = `assets/images/${image}`;
          
-              const h5 = document.createElement('h5');
-              divquantityheart.appendChild(h5);
-              h5.textContent = `${title}`;
-
-              const divspanHeart = document.createElement('div');
-              divquantityheart.appendChild(divspanHeart);
-              divspanHeart.setAttribute("class","divspanheart");
-   
-              const span = document.createElement('span');
-              divspanHeart.appendChild(span);
-              span.textContent = `${likes}`;
+            const divPhoto = document.createElement('div');
+            divPhoto.setAttribute("class","divphoto");
+            main.appendChild(divPhoto);
+            
+            const articlePhoto = document.createElement('article');
+            divPhoto.appendChild(articlePhoto);
+            
+            
+                  const imgArticle = document.createElement('img');
+                  articlePhoto.appendChild(imgArticle);
+                  imgArticle.setAttribute("class","imgarticle");
+                  imgArticle.setAttribute("src","assets/images/Rhode/Sport_2000_with_8.jpg");
+                  imgArticle.setAttribute("alt",alt);
+            
+                  
+                  const divquantityheart = document.createElement('div');
+                  divquantityheart.setAttribute("class","quantityHeart");
+                  articlePhoto.appendChild(divquantityheart);
+                  
+                       const h5 = document.createElement('h5');
+                       divquantityheart.appendChild(h5);
+                       h5.textContent = `${title}`;
          
-              const i = document.createElement('i');
-              divspanHeart.appendChild(i);
-              i.setAttribute("class","fas fa-heart");
-
-}      
+                       const divspanHeart = document.createElement('div');
+                       divquantityheart.appendChild(divspanHeart);
+                       divspanHeart.setAttribute("class","divspanheart");
+            
+                       const span = document.createElement('span');
+                       divspanHeart.appendChild(span);
+                       span.textContent = `${likes}`;
+                  
+                       const i = document.createElement('i');
+                       divspanHeart.appendChild(i);
+                       i.setAttribute("class","fas fa-heart");
+}
 
 //async function displayData(photographers) {
 
@@ -199,7 +202,9 @@ main.appendChild(sectionTrier);
 
       // Récupère les datas des photographes
      const photographers = await getPhotographers();
-      attachInformation(photographers);
+     const media = await getPhotographers();
+      attachInformationPhotograph(photographers);
+      attachInformationMedia(media);
       //displayData(photographers);
   };
   
