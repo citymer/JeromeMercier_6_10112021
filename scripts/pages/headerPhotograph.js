4799d01a47c033ea9afd80cb7fc80ca5652a270d
citymer <cityhunter8484@gmail.com>
2022-02-01 22:19:01 +0100

    creation headerPhotograph.js

---
+++ scripts/pages/headerPhotograph.js
@@ -0,0 +1,53 @@
+// Récupère et affiche les informations des photographes dans la section photograph-header ***
+function attachInformationPhotograph(photographers) {
+     
+    
+    const { name, portrait, city, country, tagline,price,alt,id} = this.filterPhotographer[0];
+      
+      const picture = `assets/photographers/${portrait}`;
+      
+      //selectionne header ***** creation du lien vers l'acceuil (index.html)
+      const header = document.querySelector('header');
+      
+      const aLienAcceuil = document.createElement('a');
+         header.appendChild(aLienAcceuil);
+         aLienAcceuil.setAttribute("href","./index.html");
+         aLienAcceuil.setAttribute("title","vers la page d'acceuil");
+         
+         const imgLogo = document.querySelector('.logo');
+         aLienAcceuil.appendChild(imgLogo);
+      
+      //SELECTIONNE la section "photograph-header" 
+      const sectionPhotographHeader = document.getElementById('photograph-header');
+      sectionPhotographHeader.setAttribute("arial-label","photographer header");
+      
+        //création "article" qui contient "h2,h3,h4"
+        const article = document.createElement('article');
+        sectionPhotographHeader.appendChild(article); 
+        article.id = id;
+        article.innerHTML = `<h2>${name}</h2>
+                             <h3>${city}, ${country}</h3>
+                             <h4>${tagline}</h4>`;
+    
+      
+      // selection du bouton "contact_button"   
+      const button = document.querySelector('.contact_button');
+      button.setAttribute("title","contactez-moi");
+      
+      //creation d' img pour la photo de profil du photographe
+      const img = document.createElement('img');
+      img.setAttribute("src", picture);
+      img.setAttribute("alt",alt);
+      
+      //positionne article avant button dans HTML
+      sectionPhotographHeader.prepend(article);
+      sectionPhotographHeader.append(button); 
+      sectionPhotographHeader.append(img);       
+      
+      //ECRIT le prix/jour dans le FOOTER
+      const divPriceDay = document.createElement('div');
+      divFooterInfo.appendChild(divPriceDay);
+      divPriceDay.setAttribute("class","divpriceday");
+      divPriceDay.innerHTML = `<p class="textpriceday">${price} €/jour</p>`; 
+  
+  }  
